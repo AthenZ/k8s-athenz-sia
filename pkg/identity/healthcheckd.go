714144e65079ba99ba505cc9222306c4e5eadcb4
taniwa <taniwa@yahoo-corp.jp>
2023-08-07 15:07:54 +0900

    Add health check server shutdown timeout
    Signed-off-by: taniwa <taniwa@yahoo-corp.jp>

--- pkg/identity/healthcheckd.go
+++ pkg/identity/healthcheckd.go
@@ -18,7 +18,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/AthenZ/k8s-athenz-sia/v3/pkg/config"
 	"github.com/AthenZ/k8s-athenz-sia/v3/third_party/log"
@@ -58,7 +57,7 @@ func Healthcheckd(idConfig *config.IdentityConfig, stopChan <-chan struct{}) (er
 		defer close(shutdownChan)
 
 		<-stopChan
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), idConfig.ShutdownTimeout)
 		defer cancel()
 		healthCheckServer.SetKeepAlivesEnabled(false)
 		if err := healthCheckServer.Shutdown(ctx); err != nil {
