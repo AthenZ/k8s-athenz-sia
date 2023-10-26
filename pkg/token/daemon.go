434798d1bd2a264e04734bd745fa1f32708222a8
Windz <WindzCUHK@users.noreply.github.com>
2023-10-26 09:56:37 +0900

    Update pkg/token/daemon.go
    Co-authored-by: Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
    Signed-off-by: Windz <WindzCUHK@users.noreply.github.com>

--- pkg/token/daemon.go
+++ pkg/token/daemon.go
@@ -274,7 +274,7 @@ func Tokend(idConfig *config.IdentityConfig, stopChan <-chan struct{}) (error, <
 					log.Errorf("Failed to refresh tokens after multiple retries: %s", err.Error())
 				}
 			case <-stopChan:
-				log.Info("Token provider will shut down...")
+				log.Info("Initiating shutdown of token provider daemon ...")
 				time.Sleep(idConfig.ShutdownDelay)
 				ctx, cancel := context.WithTimeout(context.Background(), idConfig.ShutdownTimeout)
 				defer cancel()
