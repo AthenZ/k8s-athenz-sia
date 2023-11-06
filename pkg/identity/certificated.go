3cbe937e6a52805f5f9cf7425dfc63a3403c8684
Jeongwoo Kim - jekim <jekim@yahoo-corp.jp>
2023-11-06 14:36:06 +0900

    feat: file

--- pkg/identity/certificated.go
+++ pkg/identity/certificated.go
@@ -2,6 +2,7 @@ package identity
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -193,9 +194,34 @@ func Certificated(idConfig *IdentityConfig, stopChan <-chan struct{}) (error, <-
 					keyPEM = k8sSecretBackupKeyPEM
 					log.Infof("Successfully loaded x509 certificate from kubernetes secret")
 				}
-			} else {
+			} else if idConfig.Backup == "file" {
+				log.Debugf("Attempting to load x509 certificate from local file: key[%s], cert[%s]...", idConfig.KeyFile, idConfig.CertFile)
 
-				log.Debugf("Skipping to load x509 certificate temporary backup from Kubernetes secret[%s]", idConfig.CertSecret)
+				fileBackupCertPEM, err := ioutil.ReadFile(idConfig.CertFile)
+				if err != nil {
+					log.Warnf("Error while reading x509 certificate from local file[%s]: %s", idConfig.CertFile, err.Error())
+				}
+				fileBackupKeyPEM, err := ioutil.ReadFile(idConfig.KeyFile)
+				if err != nil {
+					log.Warnf("Error while reading x509 certificate key from local file[%s]: %s", idConfig.KeyFile, err.Error())
+				}
+
+				fileBackupIdentity, err := InstanceIdentityFromPEMBytes(fileBackupCertPEM)
+				if err != nil {
+					log.Warnf("Error while parsing x509 certificate from local file: %s", err.Error())
+				}
+
+				if fileBackupIdentity == nil || len(fileBackupKeyPEM) == 0 {
+					log.Errorf("Failed to load x509 certificate from local file to get x509 role certs: key size[%d]bytes, certificate size[%d]bytes", len(fileBackupCertPEM), len(fileBackupKeyPEM))
+				} else {
+					identity = fileBackupIdentity
+					keyPEM = fileBackupKeyPEM
+					log.Debugf("Successfully loaded x509 certificate from local file to get x509 role certs: key size[%d]bytes, certificate size[%d]bytes", len(fileBackupCertPEM), len(fileBackupKeyPEM))
+				}
+			} else if idConfig.CertSecret == "" && strings.Contains(idConfig.Backup, "read") {
+				log.Debugf("Skipping to load x509 certificate temporary backup from Kubernetes secret with empty CERT_SECRET[%s]", idConfig.CertSecret)
+			} else {
+				log.Debugf("Skipping to load x509 certificate temporary backup from Kubernetes secret or local file with BACKUP[%s]", idConfig.Backup)
 			}
 		}
 
