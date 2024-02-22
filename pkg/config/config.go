10306097c48fbf787d23a1426ed5e59fe86d4d9a
Jeongwoo Kim - jekim <jekim@yahoo-corp.jp>
2024-02-22 16:27:48 +0900

    fix: small comments

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -203,7 +203,7 @@ func (idConfig *IdentityConfig) loadFromFlag(program string, args []string) erro
 	f.StringVar(&idConfig.MetricsServerAddr, "metrics-server-addr", idConfig.MetricsServerAddr, "HTTP server address to provide metrics")
 	f.BoolVar(&idConfig.DeleteInstanceID, "delete-instance-id", idConfig.DeleteInstanceID, "delete x509 certificate record from identity provider on shutdown (true/false)")
 	// Header Token Server
-	f.BoolVar(&idConfig.HeaderTokenServer, "token-server-envoy-api", idConfig.HeaderTokenServer, "enable token server Envoy API (true/false)")
+	f.BoolVar(&idConfig.HeaderTokenServer, "header-token-server", idConfig.HeaderTokenServer, "enable header token server (true/false)")
 	// log
 	f.StringVar(&idConfig.LogDir, "log-dir", idConfig.LogDir, "directory to store the log files")
 	f.StringVar(&idConfig.LogLevel, "log-level", idConfig.LogLevel, "logging level")
