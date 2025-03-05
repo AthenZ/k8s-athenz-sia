fdb9e3b9257b37ab00756d5ca986c3a39a61ceaf
wfan <wfan@lycorp.co.jp>
2025-03-05 16:36:08 +0900

    remove comments
    Signed-off-by: wfan <wfan@lycorp.co.jp>

--- pkg/config/derived-token-server.go
+++ pkg/config/derived-token-server.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package config defines all the configuration parameters. It reads configuration from environment variables and command-line arguments.
 package config
 
 import (
