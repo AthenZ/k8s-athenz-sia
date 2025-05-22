f73a9b12cd13508726f0dddda88cec53967592c5
taniwa <taniwa@lycorp.co.jp>
2025-05-22 13:47:44 +0900

    Support multiple cert and key files in certificate handling
    Refactor certificate handling to support multiple certificate and key files by splitting file paths on commas. Ensure directories are created for each file path before saving. Update cert reloader to use the first file in the list for monitoring. This enhances flexibility in certificate management.
    
    Signed-off-by: taniwa <taniwa@lycorp.co.jp>

--- pkg/certificate/service.go
+++ pkg/certificate/service.go
@@ -17,6 +17,7 @@ package certificate
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
@@ -83,13 +84,39 @@ func New(ctx context.Context, idCfg *config.IdentityConfig) (daemon.Daemon, erro
 				}
 				log.Infof("[New Instance Certificate] Subject: %s, Issuer: %s, NotBefore: %s, NotAfter: %s, SerialNumber: %s, DNSNames: %s",
 					x509Cert.Subject, x509Cert.Issuer, x509Cert.NotBefore, x509Cert.NotAfter, x509Cert.SerialNumber, x509Cert.DNSNames)
-				log.Debugf("Saving x509 cert[%d bytes] at %s", len(leafPEM), idCfg.CertFile)
-				if err := w.AddBytes(idCfg.CertFile, 0644, leafPEM); err != nil {
-					return fmt.Errorf("unable to save x509 cert: %w", err)
+				certFiles := strings.Split(idCfg.CertFile, ",")
+				keyFiles := strings.Split(idCfg.KeyFile, ",")
+
+				for _, certFile := range certFiles {
+					certFile = strings.TrimSpace(certFile)
+					if certFile == "" {
+						continue
+					}
+
+					if err := extutil.CreateDirectory(certFile); err != nil {
+						return fmt.Errorf("unable to create directory for x509 cert: %w", err)
+					}
+
+					log.Debugf("Saving x509 cert[%d bytes] at %s", len(leafPEM), certFile)
+					if err := w.AddBytes(certFile, 0644, leafPEM); err != nil {
+						return fmt.Errorf("unable to save x509 cert: %w", err)
+					}
 				}
-				log.Debugf("Saving x509 key[%d bytes] at %s", len(keyPEM), idCfg.KeyFile)
-				if err := w.AddBytes(idCfg.KeyFile, 0644, keyPEM); err != nil {
-					return fmt.Errorf("unable to save x509 key: %w", err)
+
+				for _, keyFile := range keyFiles {
+					keyFile = strings.TrimSpace(keyFile)
+					if keyFile == "" {
+						continue
+					}
+
+					if err := extutil.CreateDirectory(keyFile); err != nil {
+						return fmt.Errorf("unable to create directory for x509 key: %w", err)
+					}
+
+					log.Debugf("Saving x509 key[%d bytes] at %s", len(keyPEM), keyFile)
+					if err := w.AddBytes(keyFile, 0644, keyPEM); err != nil {
+						return fmt.Errorf("unable to save x509 key: %w", err)
+					}
 				}
 			}
 
