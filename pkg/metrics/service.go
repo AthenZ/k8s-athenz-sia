38ce8929fd9eb15e672f9c0c36603b35c7c1704f
taniwa <taniwa@lycorp.co.jp>
2025-06-04 19:02:06 +0900

    Update logging for local certificate path usage in metrics service
    Signed-off-by: taniwa <taniwa@lycorp.co.jp>

--- pkg/metrics/service.go
+++ pkg/metrics/service.go
@@ -78,7 +78,8 @@ func New(ctx context.Context, idCfg *config.IdentityConfig) (daemon.Daemon, erro
 					}
 				}
 			}
-			log.Debug("LocalCert.Use: %t, LocalCert.CertPath: %s, LocalCert.KeyPath: %s", idCfg.ServiceCert.LocalCert.Use, idCfg.ServiceCert.LocalCert.CertPath, idCfg.ServiceCert.LocalCert.KeyPath)
+			// TODO: DEBUG
+			log.Infof("LocalCert.Use: %t, LocalCert.CertPath: %s, LocalCert.KeyPath: %s", idCfg.ServiceCert.LocalCert.Use, idCfg.ServiceCert.LocalCert.CertPath, idCfg.ServiceCert.LocalCert.KeyPath)
 			if idCfg.ServiceCert.LocalCert.Use && idCfg.ServiceCert.LocalCert.CertPath != "" {
 				files = append(files, idCfg.ServiceCert.LocalCert.CertPath)
 			}
